324af25e72602601dc85251e066762ab89c67aec
Burak Tokak <iletisim@buraktokak.com>
2022-06-15 20:57:13 +0300

    jest steup

---
+++ jest.setup.js
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom/extend-expect'
+
+// hide error messages about act() being unsupported in production build
+const ignoredErrors = [
+  /act(...) is not supported in production builds of React, and might not behave as expected./,
+]
+const consoleError = global.console.error
+global.console.error = (...args) => {
+  if (ignoredErrors.some((el) => el.test(args[0]))) {
+    return consoleError(...args)
+  }
+}
